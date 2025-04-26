35fb2438140a00866a6281529486125a987c5711
lucascostamr <lucascostamr812@gmail.com>
2025-04-26 14:35:33 -0300

    refactor: update UserRepository dependencies

--- app/strategies/user_password_strategy.py
+++ app/strategies/user_password_strategy.py
@@ -1,5 +1,6 @@
 from strategies.auth_strategy_interface import AuthStrategy
 from repository.user_repository import UserRepository
+from domain.user import User
 
 
 class UserPasswordStrategy(AuthStrategy):
@@ -9,9 +10,9 @@ class UserPasswordStrategy(AuthStrategy):
     ):
         self.user_repository = user_repository
 
-    async def authenticate(self, username: str, password: str) -> dict:
-        user_data: dict = await self.user_repository.get_user_data(username)
-        return user_data
+    async def authenticate(self, username: str, password: str):
+        user_data: User = await self.user_repository.get_user_by_username(username)
+        return "token"
     
     def get_url_redirect(self) -> str:
         pass
--- app/strategies/user_password_strategy_test.py
+++ app/strategies/user_password_strategy_test.py
@@ -1,40 +1,56 @@
 from unittest.mock import AsyncMock
 
 import pytest
+from data.user.user_data_provider import UserDataProvider
+from domain.user import User
 from repository.user_repository import UserRepository
 from strategies.user_password_strategy import UserPasswordStrategy
 
+fake_user = User("fake_username", "fake_password")
+
+
+@pytest.fixture
+def user_repository():
+    return UserRepository(UserDataProvider())
+
+
+@pytest.fixture
+def sut(user_repository: UserRepository):
+    return UserPasswordStrategy(user_repository)
+
 
 @pytest.mark.asyncio
-async def test_should_return_user_data_on_success():
-    user_repository = UserRepository()
-    user_repository.get_user_data = AsyncMock(return_value={"user_data": "fake_data"})
-    sut = UserPasswordStrategy(user_repository)
+async def test_should_return_token_on_success(
+    user_repository: UserRepository, sut: UserPasswordStrategy
+):
+    user_repository.get_user_by_username = AsyncMock(value=fake_user)
 
     response = await sut.authenticate("fake_username", "fake_password")
 
-    if response != {"user_data": "fake_data"}:
+    if response != "token":
         raise AssertionError(
             f"Expected {{'user_data': 'fake_data'}}, but got {response}"
         )
 
 
 @pytest.mark.asyncio
-async def test_should_throw_on_user_not_found():
-    user_repository = UserRepository()
-    user_repository.get_user_data = AsyncMock(side_effect=Exception("User not found"))
-    sut = UserPasswordStrategy(user_repository)
+async def test_should_throw_on_user_not_found(
+    user_repository: UserRepository, sut: UserPasswordStrategy
+):
+    user_repository.get_user_by_username = AsyncMock(
+        side_effect=Exception("User not found")
+    )
 
     with pytest.raises(Exception, match="User not found"):
         await sut.authenticate("fake_username", "fake_password")
 
 
 @pytest.mark.asyncio
-async def test_should_call_get_user_data_with_correct_params():
-    user_repository = UserRepository()
-    user_repository.get_user_data = AsyncMock(return_value={"user_data": "fake_data"})
-    sut = UserPasswordStrategy(user_repository)
+async def test_should_call_get_user_by_username_with_correct_params(
+    user_repository: UserRepository, sut: UserPasswordStrategy
+):
+    user_repository.get_user_by_username = AsyncMock(return_value=fake_user)
 
     await sut.authenticate("fake_username", "fake_password")
 
-    user_repository.get_user_data.assert_awaited_once_with("fake_username")
+    user_repository.get_user_by_username.assert_awaited_once_with("fake_username")
