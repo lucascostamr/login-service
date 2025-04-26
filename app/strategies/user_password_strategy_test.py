269f78ac1fb3006e2c48a99b5aa05fd25365ca61
lucascostamr <lucascostamr812@gmail.com>
2025-04-26 11:28:05 -0300

    test: ensure UserPasswordStrategy throws if no user found

--- app/strategies/user_password_strategy_test.py
+++ app/strategies/user_password_strategy_test.py
@@ -8,9 +8,18 @@ from strategies.user_password_strategy import UserPasswordStrategy
 @pytest.mark.asyncio
 async def test_should_return_user_data_on_success():
     user_repository = UserRepository()
-    user_repository.get_user_data = AsyncMock(return_value={"username": "test_user"})
+    user_repository.get_user_data = AsyncMock(return_value={"user_data": "fake_data"})
     sut = UserPasswordStrategy(user_repository)
 
-    response = await sut.authenticate("test_user", "test_password")
+    response = await sut.authenticate("fake_username", "fake_password")
 
-    assert response == {"username": "test_user"}
+    assert response == {"user_data": "fake_data"}
+
+@pytest.mark.asyncio
+async def test_should_throw_on_user_not_found():
+    user_repository = UserRepository()
+    user_repository.get_user_data = AsyncMock(side_effect=Exception("User not found"))
+    sut = UserPasswordStrategy(user_repository)
+
+    with pytest.raises(Exception, match="User not found"):
+        await sut.authenticate("fake_username", "fake_password")
